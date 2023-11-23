606aab9f1e8d3398b66b36ce78d4f365aa220734
Rajab-Aboobacker26 <148728462+Rajab-Aboobacker26@users.noreply.github.com>
2023-11-22 22:34:54 -0500

    Add files via upload

--- DietTrackingApp/src/ExerciseLog.java
+++ DietTrackingApp/src/ExerciseLog.java
@@ -1,8 +1,14 @@
 
 
 import java.awt.event.ActionEvent;
+
+
 import java.awt.event.ActionListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Vector;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -10,165 +16,169 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+
 public class ExerciseLog extends javax.swing.JFrame implements ActionListener{
-
-	JFrame frame = new JFrame("Exercise Log");
-	JTextField fDate = new JTextField();
-	JTextField fMonth = new JTextField();
-	JTextField fYear = new JTextField();
-	JTextField exerciseDuration = new JTextField();
-	JComboBox<String> typesExercises;
-	JComboBox<String> intensityType;
-	JButton Save = new JButton("Save");
-	JButton Back = new JButton("Back");
-	private static final long serialVersionUID = 1L;
-	private User user;
-
-
-	public ExerciseLog(User user) {
-		this.user = user;
-	}
-	public void ExerciseLogUI() {
-		JLabel typeofExercise = new JLabel("What exercise do you want to log?");
-		typeofExercise.setBounds(30, 10, 400, 100);
-
-		Vector<String> exercises = new Vector<String>();
-		exercises.add("walking");
-		exercises.add("jogging");
-		exercises.add("bicycling");
-		exercises.add("cleaning");
-		exercises.add("dishwashing");
-		exercises.add("sitting");
-		exercises.add("weight_training");
-		exercises.add("mowing_lawn");
-		exercises.add("football");
-		exercises.add("basketball");
-
-		typesExercises = new JComboBox<String>(exercises);
-		typesExercises.setBounds(30, 80, 100, 20);
-		typesExercises.addActionListener(this);
-
-		JLabel dateofExercise = new JLabel("Give the date you exercised (YYYY/MM/DD): ");
-		dateofExercise.setBounds(30, 80, 400, 100);
+	
+JFrame frame = new JFrame("Exercise Log");
+JTextField fDate = new JTextField();
+JTextField fMonth = new JTextField();
+JTextField fYear = new JTextField();
+JTextField exerciseDuration = new JTextField();
+JComboBox<String> typesExercises;
+JComboBox<String> intensityType;
+JButton Save = new JButton("Save");
+JButton Back = new JButton("Back");
+private static final long serialVersionUID = 1L;
+private User user;
 
 
-		JLabel fromLabel = new JLabel("Date:");
-		fromLabel.setBounds(30, 145, 40, 20);
+public ExerciseLog(User user) {
+	this.user = user;	
+}
+public void ExerciseLogUI() {
+	JLabel typeofExercise = new JLabel("What exercise do you want to log?");
+	typeofExercise.setBounds(30, 10, 400, 100);
+	
+	Vector<String> exercises = new Vector<String>();
+	exercises.add("walking");
+	exercises.add("jogging");
+	exercises.add("bicycling");
+	exercises.add("cleaning");
+	exercises.add("dishwashing");
+	exercises.add("sitting");
+	exercises.add("weight_training");
+	exercises.add("mowing_lawn");
+	exercises.add("football");
+	exercises.add("basketball");
 
-		fYear.setBounds(30, 170, 40, 25);
-		JLabel fYearSlash = new JLabel("/");
-		fYearSlash.setBounds(70, 170, 30, 25);
+	typesExercises = new JComboBox<String>(exercises);
+	typesExercises.setBounds(30, 80, 100, 20);
+	typesExercises.addActionListener(this);
+	
+	JLabel dateofExercise = new JLabel("Give the date you exercised (YYYY/MM/DD): ");
+	dateofExercise.setBounds(30, 80, 400, 100);
+	
 
-		fMonth.setBounds(80, 170, 30, 25);
-		JLabel fMonthSlash = new JLabel("/");
-		fMonthSlash.setBounds(110, 170, 30, 25);
+	JLabel fromLabel = new JLabel("Date:");
+	fromLabel.setBounds(30, 145, 40, 20);
+	
+	fYear.setBounds(30, 170, 40, 25);
+	JLabel fYearSlash = new JLabel("/");
+	fYearSlash.setBounds(70, 170, 30, 25);
+	
+	fMonth.setBounds(80, 170, 30, 25);
+	JLabel fMonthSlash = new JLabel("/");
+	fMonthSlash.setBounds(110, 170, 30, 25);
+	
+	fDate.setBounds(120, 170, 30, 25);
 
-		fDate.setBounds(120, 170, 30, 25);
+	JLabel DurationLabel = new JLabel("Duration (in hours):");
+	DurationLabel.setBounds(175, 145, 150, 20);
+	
+	exerciseDuration.setBounds(175, 170, 40, 30);
+	
+	
+	JLabel intensityofExercise = new JLabel("How would you describe your intensity during this period?");
+	intensityofExercise.setBounds(30, 165, 400, 100);
+	
+	Vector<String> intensity = new Vector<String>();
+	intensity.add("Easy");
+	intensity.add("Medium");
+	intensity.add("Hard");
+	intensity.add("Very hard");	
+	
+	intensityType = new JComboBox<>(intensity);
+	intensityType.setBounds(30, 235, 100, 20);
+	intensityType.addActionListener(this);
+	
+	Save.setBounds(30, 300, 90, 20);
+	Save.addActionListener(this);
+    
+	Back.setBounds(180, 300, 90, 20);
+	Back.addActionListener(this);
 
-		JLabel DurationLabel = new JLabel("Duration (in hours):");
-		DurationLabel.setBounds(175, 145, 150, 20);
-
-		exerciseDuration.setBounds(175, 170, 40, 30);
-
-
-		JLabel intensityofExercise = new JLabel("How would you describe your intensity during this period?");
-		intensityofExercise.setBounds(30, 165, 400, 100);
-
-		Vector<String> intensity = new Vector<String>();
-		intensity.add("Easy");
-		intensity.add("Medium");
-		intensity.add("Hard");
-		intensity.add("Very hard");
-
-		intensityType = new JComboBox<>(intensity);
-		intensityType.setBounds(30, 235, 100, 20);
-		intensityType.addActionListener(this);
-
-		Save.setBounds(30, 300, 90, 20);
-		Save.addActionListener(this);
-
-		Back.setBounds(180, 300, 90, 20);
-		Back.addActionListener(this);
-
-
-
-		//this piece of code adds the element to the UI window
-		frame.add(typeofExercise);
-		frame.add(typesExercises);
-		frame.add(dateofExercise);
-		frame.add(fDate);
-		frame.add(fMonth);
-		frame.add(fYear);
-		frame.add(fromLabel);
-		frame.add(fYearSlash);
-		frame.add(fMonthSlash);
-		frame.add(DurationLabel);
-		frame.add(exerciseDuration);
-		frame.add(intensityofExercise);
-		frame.add(intensityType);
-		frame.add(Save);
-		frame.add(Back);
-
+    
+    
+    //this piece of code adds the element to the UI window
+	frame.add(typeofExercise);
+	frame.add(typesExercises);
+	frame.add(dateofExercise);
+	frame.add(fDate);
+	frame.add(fMonth);
+	frame.add(fYear);
+	frame.add(fromLabel);
+	frame.add(fYearSlash);
+	frame.add(fMonthSlash);
+	frame.add(DurationLabel);
+	frame.add(exerciseDuration);
+	frame.add(intensityofExercise);
+	frame.add(intensityType);
+	frame.add(Save);
+	frame.add(Back);  
+	
 //	frame.pack();
+    
+    //need this piece of code for the window to pop up
+    
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(400,400);
+	frame.setLayout(null);
+	frame.setVisible(true);
+}
+public void actionPerformed(ActionEvent e){   
 
-		//need this piece of code for the window to pop up
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,400);
-		frame.setLayout(null);
-		frame.setVisible(true);
-	}
-	public void actionPerformed(ActionEvent e){
-
-		if(e.getSource() == Save) {
-			try {
-				int fromY, fromM, fromD;
-				double durationbyHour;
-				String intensityLevel;
-				String exerciseActivity;
-				fromY = Integer.parseInt(fYear.getText());
-				fromM = Integer.parseInt(fMonth.getText());
-				fromD = Integer.parseInt(fDate.getText());
-				durationbyHour = Double.parseDouble(exerciseDuration.getText());
-				intensityLevel = intensityType.getSelectedItem().toString();
-				exerciseActivity = typesExercises.getSelectedItem().toString();
-
-				System.out.println("\nDATE: "+fromY+"/"+fromM+"/"+fromD+" duration: "+durationbyHour+" intensity: "
-						+intensityLevel+" exercise: "+exerciseActivity);
-
-				String date;
-
-				if(fYear.getText().length() == 4 && fMonth.getText().length() <= 2 && fDate.getText().length() <= 2) {
-					if(fromM <= 12 && fromD <=31 && durationbyHour < 24) {
-						System.out.println("Date is formatted properly");
-						date = fYear.getText()+"-"+fMonth.getText()+"-"+fDate.getText();
-						ExerciseUser Euser = new ExerciseUser(user, date, durationbyHour, exerciseActivity, intensityLevel);
-						Euser.insertExerciseData();
-						Euser.insertCaloriesBurned();
-						System.out.println(date);
-
-						JOptionPane.showMessageDialog(this, "Your exercise log was recorded!");
-						dispose();
-						AccountHomePageUI a = new AccountHomePageUI();
-						frame.dispose();
-						a.setVisible(true);
-					}
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "Please retry", "alert", JOptionPane.ERROR_MESSAGE);
-				}
-			}catch(Exception exc) {
-				JOptionPane.showMessageDialog(null, "Please retry", "alert", JOptionPane.ERROR_MESSAGE);
-				exc.printStackTrace();
-			}
-		}
-		else if(e.getSource() == Back) {
-			AccountHomePageUI a = new AccountHomePageUI();
-			frame.setVisible(false);
-			frame.dispose();
-			a.setVisible(true);
-
+	if(e.getSource() == Save) {
+		try {
+			int fromY, fromM, fromD;
+			double durationbyHour;
+			String intensityLevel;
+			String exerciseActivity; 
+			 fromY = Integer.parseInt(fYear.getText());
+			 fromM = Integer.parseInt(fMonth.getText());
+			 fromD = Integer.parseInt(fDate.getText());	 
+			 durationbyHour = Double.parseDouble(exerciseDuration.getText());
+			 intensityLevel = intensityType.getSelectedItem().toString();
+			 exerciseActivity = typesExercises.getSelectedItem().toString();
+			 
+			 System.out.println("\nDATE: "+fromY+"/"+fromM+"/"+fromD+" duration: "+durationbyHour+" intensity: "
+			 +intensityLevel+" exercise: "+exerciseActivity);
+			 System.out.println("name: "+user.getFirstName()+"sex: "+user.getSex());
+			 
+			 String strDate;
+			 
+			 if(fYear.getText().length() == 4 && fMonth.getText().length() <= 2 && fDate.getText().length() <= 2) {
+				 if(fromM <= 12 && fromD <=31 && durationbyHour < 24) {
+					 System.out.println("Date is formatted properly");
+				      
+					 SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");      
+					 strDate = fDate.getText()+"-"+fMonth.getText()+"-"+fYear.getText();
+					 Date date = new Date();
+					 date = formatter.parse(strDate);
+					 ExerciseUser Euser = new ExerciseUser(user, date, durationbyHour, exerciseActivity, intensityLevel);
+					 Euser.insertExerciseData();
+					 System.out.println(date);
+					 
+					 JOptionPane.showMessageDialog(this, "Your exercise log was recorded!");
+					 dispose();
+					 AccountHomePageUI a = new AccountHomePageUI(this.user);
+					 frame.dispose();
+					 a.setVisible(true); 
+				 }
+			 }
+			 else {
+				 JOptionPane.showMessageDialog(null, "Please retry", "alert", JOptionPane.ERROR_MESSAGE);
+			 }	 
+		}catch(Exception exc) {
+			JOptionPane.showMessageDialog(null, "Please retry", "alert", JOptionPane.ERROR_MESSAGE);			
+			exc.printStackTrace();
 		}
 	}
+	else if(e.getSource() == Back) {
+		AccountHomePageUI a = new AccountHomePageUI(this.user);
+		frame.setVisible(false);
+		frame.dispose();
+		a.setVisible(true);	
+	}
+}
 
 }
--- DietTrackingApp/src/ExerciseUser.java
+++ DietTrackingApp/src/ExerciseUser.java
@@ -1,72 +1,99 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-public class ExerciseUser extends User{
-    private String date;
-    private String intensity;
-    private String exercise;
-    private double duration;
-    private int exerciseID;
+import java.util.Date;
 
-    public ExerciseUser(User user, String date, Double duration, String exercise, String intensity) {
-        super(user.accID, user.firstName, user.lastName, user.age, user.weight, user.height, user.sex, user.units);
-        exerciseID = super.accID;
-        this.exercise = exercise;
-        this.intensity = intensity;
-        this.duration = duration;
-        this.date = date;
-    }
-    public String getIntensity() {
-        return intensity;
-    }
-    public Double getDuration() {
-        return duration;
-    }
-    public String getExercise() {
-        return exercise;
-    }
-    public String getDate() {
-        return date;
-    }
-    public void insertExerciseData() throws SQLException {
-        SQLConnection sqlConnect = new SQLConnection();
-        Connection connect = sqlConnect.SQLConnect();
+public class ExerciseUser extends User {
+	private Date date;
+	private String intensity;
+	private String exercise;
+	private double duration;
+	private int totalCaloriesBurned;
+	private int account_id;
+	
+	public ExerciseUser(User user, Date date, Double duration, String exercise, String intensity) {
+		super(user.accID, user.firstName, user.lastName, user.age, user.weight, user.height, user.sex, user.units);
+		account_id = user.getAccID();
+		this.exercise = exercise;
+		this.intensity = intensity;
+		this.duration = duration;
+		this.date = date;
+	}
+	public String getIntensity() {
+		return intensity;
+	}
+	public Double getDuration() {
+		return duration;
+	}
+	public String getExercise() {
+		return exercise;
+	}
+	public Date getDate() {
+		return date;
+	}
 
-        String sql = "INSERT INTO exercise_log(exercise_date,exercise_intensity,exercise_type,exercise_duration,exercise_ID) VALUES (?,?,?,?,?)";
-
-        PreparedStatement preparedStatement = connect.prepareStatement(sql);
-        preparedStatement.setString(1, date);
-        preparedStatement.setString(2, intensity);
-        preparedStatement.setString(3, exercise);
-        preparedStatement.setDouble(4, duration);
-        preparedStatement.setInt(5, exerciseID);
-
-        int rows = preparedStatement.executeUpdate();
-        if(rows > 0) {
-            System.out.println("Exercise data logged in");
-        }
-    }
-    public void insertCaloriesBurned() throws SQLException{
+	public void insertExerciseData() throws SQLException {
         SQLConnection sqlConnect = new SQLConnection();
         Connection connect = sqlConnect.SQLConnect();
 
         CalorieCalculator newCalc = new CalorieCalculator();
-        double totalCaloriesBurnt = newCalc.calculateCaloriesBurned(this);
+        totalCaloriesBurned = (int)newCalc.calculateCaloriesBurned(this);
+        
+		String sqlQuery = "SELECT * FROM exercise_log WHERE exercise_date = ?";
+		PreparedStatement preparedStatement = connect.prepareStatement(sqlQuery);
+        java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+		preparedStatement.setDate(1, sqlDate);
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
 
-        String sql = "UPDATE exercise_log SET calories_burned = ? WHERE exercise_ID = ?";
-        PreparedStatement preparedStatement = connect.prepareStatement(sql);
-        preparedStatement = connect.prepareStatement(sql);
-        preparedStatement.setDouble(1,totalCaloriesBurnt);
-        preparedStatement.setInt(2,this.exerciseID);
+		if(resultSet.next()) {
+			   ResultSetMetaData rsmd = resultSet.getMetaData();
+			   System.out.println("\nquerying SELECT * FROM XXX");
+			   int columnsNumber = rsmd.getColumnCount();
+			   while (resultSet.next()) {
+			       for (int i = 1; i <= columnsNumber; i++) {
+			           if (i > 1) System.out.print(",  ");
+			           String columnValue = resultSet.getString(i);
+			           System.out.print(columnValue + " " + rsmd.getColumnName(i));
+			       }
+			       System.out.println("");
+			   }
+			   
+			String sumQuery = "SELECT SUM(calories_burned) AS total_calories FROM exercise_log WHERE exercise_date = ?";
+			String updateStatement = "UPDATE exercise_log SET calories_burned = ? WHERE exercise_date = ?";
+			// Create a Statement and a PreparedStatement
+			PreparedStatement sumPreparedStatement = connect.prepareStatement(sumQuery);
+			PreparedStatement updatePreparedStatement = connect.prepareStatement(updateStatement);
+			
+			sumPreparedStatement.setDate(1, sqlDate);
+			ResultSet resultSet2 = sumPreparedStatement.executeQuery();
+			
+			int totalCalories = totalCaloriesBurned;
+			if(resultSet2.next()) {
+			    totalCalories += resultSet2.getInt("total_calories");
+			}
+			
+			// Step 2: Update the row with the calculated sum
+			updatePreparedStatement.setInt(1, totalCalories);
+			updatePreparedStatement.setDate(2, sqlDate);
+			updatePreparedStatement.executeUpdate();
+		}
+		else {
+			String sql = "INSERT INTO exercise_log(AccountID,exercise_date,exercise_intensity,exercise_type,exercise_duration,calories_burned) VALUES (?,?,?,?,?,?)";
+			preparedStatement = connect.prepareStatement(sql);
 
-        int result = preparedStatement.executeUpdate();
-
-        if (result > 0) {
-            System.out.println("Updated calories burnt");
-        }
-        else {
-            System.out.println("Unable to add calories burnt");
-        }
-
-    }
+			preparedStatement = connect.prepareStatement(sql);
+	        preparedStatement.setInt(1, account_id);
+	        preparedStatement.setDate(2, sqlDate);
+	        preparedStatement.setString(3, intensity);
+	        preparedStatement.setString(4, exercise);
+	        preparedStatement.setDouble(5, duration);
+	        preparedStatement.setInt(6, totalCaloriesBurned);
+	        
+			preparedStatement.executeUpdate();
+		}            
+	}
+	
 }
